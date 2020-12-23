8dda47e9eb0d9f39c8cb07985d611b67d8147fa8
刀客 <744252461@qq.com>
2020-12-23 15:13:20 +0800

    提交项目文件

---
+++ CefSharpWinform/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace CefSharpWinform
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.txt_Keywords = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_keyperson = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.txt_keyperson);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.checkBox1);
+            this.groupBox1.Controls.Add(this.txt_Keywords);
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(1337, 137);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "输入框";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(1183, 44);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(120, 16);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "是否开启自动功能";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.button1_Click);
+            // 
+            // txt_Keywords
+            // 
+            this.txt_Keywords.Location = new System.Drawing.Point(70, 14);
+            this.txt_Keywords.Multiline = true;
+            this.txt_Keywords.Name = "txt_Keywords";
+            this.txt_Keywords.Size = new System.Drawing.Size(970, 59);
+            this.txt_Keywords.TabIndex = 2;
+            this.txt_Keywords.Text = "请问有哪位律师有空|";
+            this.txt_Keywords.TextChanged += new System.EventHandler(this.txt_Keywords_TextChanged);
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Location = new System.Drawing.Point(13, 156);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1337, 518);
+            this.panel1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "关键词";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 86);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "关注人";
+            // 
+            // txt_keyperson
+            // 
+            this.txt_keyperson.Location = new System.Drawing.Point(71, 82);
+            this.txt_keyperson.Multiline = true;
+            this.txt_keyperson.Name = "txt_keyperson";
+            this.txt_keyperson.Size = new System.Drawing.Size(969, 49);
+            this.txt_keyperson.TabIndex = 6;
+            this.txt_keyperson.TextChanged += new System.EventHandler(this.txt_keyperson_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1362, 677);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "微信";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox txt_Keywords;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.TextBox txt_keyperson;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ CefSharpWinform/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CefSharp;
+using CefSharp.WinForms;
+
+namespace CefSharpWinform
+{
+    public partial class Form1 : Form
+    {
+        string url = "https://wx.qq.com/";
+        ChromiumWebBrowser webview;
+        Timer timer = new Timer();
+        bool isfirsttime = true;
+        int count = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            if (File.Exists("./Keywords.txt") && File.ReadAllText("./Keywords.txt").Length > 0)
+                txt_Keywords.Text = File.ReadAllText("./Keywords.txt");
+            if (File.Exists("./Keypersons.txt") && File.ReadAllText("./Keypersons.txt").Length > 0)
+                txt_keyperson.Text = File.ReadAllText("./Keypersons.txt");
+
+            webview = new ChromiumWebBrowser(url);
+            webview.Dock = DockStyle.Fill;
+            this.panel1.Controls.Add(webview);
+            timer.Interval = 5000;
+            timer.Tick += Timer_Tick;
+            timer.Enabled = false;
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            if (isfirsttime)
+            {
+                timer.Interval = 500;
+                isfirsttime = false; return;
+            }
+            object EvaluateJavaScriptResult = new object();
+            var script = @"var dataArray= $('.msg');
+                for (var i = 0; i < dataArray.length; i++){
+                        var keywords='" + txt_Keywords.Text.Trim() + @"';
+                        var keypersons='"+txt_keyperson.Text.Trim()+ @"';
+                        var strs = keywords.split('|');
+                        var persons=keypersons.split('|');
+                        for(k=0;k<strs.length;k++)
+                            if (dataArray[i].innerText.search(strs[k])>=0&&strs[k].length>0)
+                            {   console.log(dataArray[i].outerText);
+                                console.log(dataArray[i].innerText);
+                                for(j=0;j<persons.length;j++)
+                                if (dataArray[i].innerText.search(persons[j])>=0&&persons[j].length>0)
+                            {$('.msg')[i].click();" +
+                            "$('#editArea').text('我');" +
+                             "angular.element(document.getElementsByClassName('btn btn_send')[0]).triggerHandler('click');" +
+                             "if(i>0)$('.chat_item .info')[0].click();else $('.chat_item .info')[1].click();" +
+                             "$('.msg')[i].click();}}};";
+            if (count % 100 == 0 || count % 333 == 0 || count % 555 == 0 || count % 777 == 0)
+            {
+                Random ran = new Random();
+                int RandKey = ran.Next(100, 999);
+                //if (RandKey % 2 == 0)
+                //    script += "$('.chat_item .info')[1].click();$('.chat_item .info')[0].click();";
+            }
+            var task = webview.GetBrowser().MainFrame.EvaluateScriptAsync(script, null);
+
+            task.ContinueWith(t =>
+            {
+                if (!t.IsFaulted)
+                {
+                    var response = t.Result;
+                    EvaluateJavaScriptResult = response.Success ? (response.Result ?? "null") : response.Message;
+                }
+            }, TaskScheduler.FromCurrentSynchronizationContext());
+            count++;
+            if (count > 100000) count = 0;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                object EvaluateJavaScriptResult = new object();
+                var script = @"if($('.chat_item .info').length>0)$('.chat_item .info')[0].click(); ";
+                var task = webview.GetBrowser().MainFrame.EvaluateScriptAsync(script, null);
+                task.ContinueWith(t =>
+                {
+                    if (!t.IsFaulted)
+                    {
+                        var response = t.Result;
+                        EvaluateJavaScriptResult = response.Success ? (response.Result ?? "null") : response.Message;
+                    }
+                }, TaskScheduler.FromCurrentSynchronizationContext());
+                timer.Enabled = true;
+            }
+            else
+            {
+                timer.Enabled = false;
+            }
+
+        }
+
+        private void txt_Keywords_TextChanged(object sender, EventArgs e)
+        {
+            File.WriteAllLines("./Keywords.txt", new List<string> { txt_Keywords.Text });
+        }
+
+        private void txt_keyperson_TextChanged(object sender, EventArgs e)
+        {
+            File.WriteAllLines("./Keypersons.txt", new List<string> { txt_keyperson.Text });
+        }
+    }
+}
